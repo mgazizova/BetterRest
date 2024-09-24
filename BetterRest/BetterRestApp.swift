fb2df1d12b4da56f7a63cc48641d325b4d7f1af7
Mariia Gazizova <mgazizova5@gmail.com>
2024-09-24 10:16:00 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Мария Газизова on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
